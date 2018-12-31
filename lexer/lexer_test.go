5eabe44fb5e7b5b6acb562772e2b0120711c294f
okada <kirinnremonn1122@gmail.com>
2019-01-01 01:43:59 +0900

    tokenを読み進めるメソッド NextToken() token.Token の実装

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -1,41 +1,40 @@
 package lexer
 
 import (
-	"fmt"
 	"testing"
+
+	"github.com/okdmm/monkey/token"
 )
 
 func TestNextToken(t *testing.T) {
 	input := `=+(){},;`
 
-	//tests := []struct {
-	//	expectedType    token.TokenType
-	//	expectedLiteral string
-	//}{
-	//	{token.ASSIGN, "="},
-	//	{token.PLUS, "+"},
-	//	{token.LPAREN, "("},
-	//	{token.RPAREN, ")"},
-	//	{token.LBRACE, "{"},
-	//	{token.RBRACE, "}"},
-	//	{token.COMMA, ","},
-	//	{token.SEMICOLON, ";"},
-	//	{token.EOF, ""},
-	//}
+	tests := []struct {
+		expectedType    token.TokenType
+		expectedLiteral string
+	}{
+		{token.ASSIGN, "="},
+		{token.PLUS, "+"},
+		{token.LPAREN, "("},
+		{token.RPAREN, ")"},
+		{token.LBRACE, "{"},
+		{token.RBRACE, "}"},
+		{token.COMMA, ","},
+		{token.SEMICOLON, ";"},
+		{token.EOF, ""},
+	}
 
 	l := New(input)
-	fmt.Println(l)
 
-	//for i, tt := range tests {
-	//	tok := L.NextToken()
+	for i, tt := range tests {
+		tok := l.NextToken()
 
-	//	if tok.Type != tt.expectedType {
-	//		t.Fatalf("test[%d] - literal wrong. expected = %q, got=%q", i, tt.expectedType, tok.Type)
-	//	}
+		if tok.Type != tt.expectedType {
+			t.Fatalf("test[%d] - literal wrong. expected = %q, got=%q", i, tt.expectedType, tok.Type)
+		}
 
-	//	if tok.Literal != tt.expectedLiteral {
-	//		t.Fatalf("test[%d] - literal wrong. expected = %q, got=%q", i, tt.expectedLiteral, tok.Literal)
-	//	}
-	//}
-
+		if tok.Literal != tt.expectedLiteral {
+			t.Fatalf("test[%d] - literal wrong. expected = %q, got=%q", i, tt.expectedLiteral, tok.Literal)
+		}
+	}
 }
