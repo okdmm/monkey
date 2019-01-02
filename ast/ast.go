c1ef068221aad9a0762d4c5f4fa507b2921eb027
okada <kirinnremonn1122@gmail.com>
2019-01-03 04:32:13 +0900

    finish Integer Literal

--- ast/ast.go
+++ ast/ast.go
@@ -123,3 +123,16 @@ func (es *ExpressionStatement) String() string {
 	}
 	return ""
 }
+
+type IntegerLiteral struct {
+	Token token.Token
+	Value int64
+}
+
+func (il *IntegerLiteral) ExpressionNode() {}
+func (il *IntegerLiteral) TokenLiteral() string {
+	return il.Token.Literal
+}
+func (il *IntegerLiteral) String() string {
+	return il.Token.Literal
+}
