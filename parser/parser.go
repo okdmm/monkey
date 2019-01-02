dbb01894993617bd7e34afe21fd51c63b0025cd0
okada <kirinnremonn1122@gmail.com>
2019-01-02 22:56:36 +0900

    add error check

--- parser/parser.go
+++ parser/parser.go
@@ -1,20 +1,25 @@
 package parser
 
 import (
+	"fmt"
+
 	"github.com/okdmm/monkey/ast"
 	"github.com/okdmm/monkey/lexer"
 	"github.com/okdmm/monkey/token"
 )
 
 type Parser struct {
-	l *lexer.Lexer
-
+	l         *lexer.Lexer
+	errors    []string
 	curToken  token.Token
 	peekToken token.Token
 }
 
 func New(l *lexer.Lexer) *Parser {
-	p := &Parser{l: l}
+	p := &Parser{
+		l:      l,
+		errors: []string{},
+	}
 
 	// 二つのトークンを読み込む。 curToken & peekToken がセットされる
 
@@ -87,6 +92,16 @@ func (p *Parser) expectPeek(t token.TokenType) bool {
 		p.nextToken()
 		return true
 	} else {
+		p.peekError(t)
 		return false
 	}
+}
+
+func (p *Parser) Errors() []string {
+	return p.errors
+}
+
+func (p *Parser) peekError(t token.TokenType) {
+	msg := fmt.Sprintf("expected next token to be %s, got %s instead", t, p.peekToken.Type)
+	p.errors = append(p.errors, msg)
 }
--- parser/parser_test.go
+++ parser/parser_test.go
@@ -9,7 +9,7 @@ import (
 
 func TestLetStatements(t *testing.T) {
 	input := `
-let x = 5;
+let x  5;
 let y = 10;
 let foobar = 82233;
 	`
@@ -18,6 +18,7 @@ let foobar = 82233;
 	p := New(l)
 
 	program := p.ParseProgram()
+	checkParserErrors(t, p)
 	if program == nil {
 		t.Fatalf("PraseProgram() returned nil")
 	}
@@ -40,6 +41,20 @@ let foobar = 82233;
 			return
 		}
 	}
+}
+
+func checkParserErrors(t *testing.T, p *Parser) {
+	errors := p.Errors()
+	if len(errors) == 0 {
+		return
+	}
+
+	t.Errorf("parser has %d error", len(errors))
+	for _, msg := range errors {
+		t.Errorf("parser error: %q", msg)
+	}
+
+	t.FailNow()
 }
 
 func testLetStatement(t *testing.T, s ast.Statement, name string) bool {
