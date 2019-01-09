26f7655af28c3be85a9f3ee7f50f13abcc8d0e29
okada <kirinnremonn1122@gmail.com>
2019-01-09 15:35:28 +0900

    add Eval String + String

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -223,9 +223,26 @@ func evalInfixExpression(operator string, left object.Object, right object.Objec
 		return nativeBoolToBooleanObject(left != right)
 	case left.Type() != right.Type():
 		return newError("type mismatch: %s %s %s", left.Type(), operator, right.Type())
+	case left.Type() == object.STRING_OBJ && right.Type() == object.STRING_OBJ:
+		return evalStringInfixExpression(operator, left, right)
 	default:
 		return newError("unknown operator: %s %s %s", left.Type(), operator, right.Type())
 	}
+}
+
+func evalStringInfixExpression(
+	operator string,
+	left, right object.Object,
+) object.Object {
+	if operator != "+" {
+		return newError("unknown operator: %s %s %s",
+			left.Type(), operator, right.Type())
+	}
+
+	leftVal := left.(*object.String).Value
+	rightVal := right.(*object.String).Value
+	return &object.String{Value: leftVal + rightVal}
+
 }
 
 func evalIntegerInfixExpression(operator string, left object.Object, right object.Object) object.Object {
--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -183,6 +183,10 @@ func TestErrorHandling(t *testing.T) {
 			"unknown operator: BOOLEAN + BOOLEAN",
 		},
 		{
+			`"Hello" - "World"`,
+			"unknown operator: STRING - STRING",
+		},
+		{
 			`
 			if (10 > 1) {
 				if (10 > 1) {
@@ -296,5 +300,18 @@ func TestStringLiteral(t *testing.T) {
 
 	if str.Value != "Hello World" {
 		t.Errorf("String was wrong Value. got=%q", str.Value)
+	}
+}
+
+func TestStringConcatenation(t *testing.T) {
+	input := `"Hello" + " " + "World!"`
+
+	evaluated := testEval(input)
+	str, ok := evaluated.(*object.String)
+	if !ok {
+		t.Fatalf("object is not String. got=%T (%+v)", evaluated, evaluated)
+	}
+	if str.Value != "Hello World!" {
+		t.Errorf("Stinr gas wrong value. got=%q", str.Value)
 	}
 }
