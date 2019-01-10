b3e403b03c485a6845c223d677dcd2b21f21b4db
okada <kirinnremonn1122@gmail.com>
2019-01-10 19:40:36 +0900

    finish len()

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -91,14 +91,18 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 }
 
 func applyFunction(fn object.Object, args []object.Object) object.Object {
-	function, ok := fn.(*object.Function)
-	if !ok {
+	switch fn := fn.(type) {
+
+	case *object.Function:
+		expectedEnv := extendFunctionEnv(fn, args)
+		evaluated := Eval(fn.Body, expectedEnv)
+		return unwrapRerurnValue(evaluated)
+	case *object.Builtin:
+		return fn.Fn(args...)
+	default:
 		return newError("not a function: %s", fn.Type())
 	}
 
-	expectedEnv := extendFunctionEnv(function, args)
-	evaluated := Eval(function.Body, expectedEnv)
-	return unwrapRerurnValue(evaluated)
 }
 
 func extendFunctionEnv(
@@ -143,12 +147,15 @@ func evalIdentifier(
 	node *ast.Identifier,
 	env *object.Environment,
 ) object.Object {
-	val, ok := env.Get(node.Value)
-	if !ok {
-		return newError("identifier not found: " + node.Value)
+	if val, ok := env.Get(node.Value); ok {
+		return val
 	}
 
-	return val
+	if builtin, ok := builtins[node.Value]; ok {
+		return builtin
+	}
+
+	return newError("Identifier not found: " + node.Value)
 }
 
 func evalBlockStatement(block *ast.BlockStatement, env *object.Environment) object.Object {
