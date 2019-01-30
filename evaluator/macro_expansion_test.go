86d00d85ca23be5015db0b7875df6d69c3028ca9
okada <kirinnremonn1122@gmail.com>
2019-01-30 22:47:10 +0900

    finish macro_expansion

--- evaluator/macro_expansion_test.go
+++ evaluator/macro_expansion_test.go
@@ -69,3 +69,39 @@ func testParseProgram(input string) *ast.Program {
 	p := parser.New(l)
 	return p.ParseProgram()
 }
+
+func TestExpandMacro(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected string
+	}{
+		{
+			`let infixExpression = macro() { quote(1 + 2); };
+			
+			infixExpression();
+			`,
+			`(1 + 2)`,
+		},
+		{
+			`
+			let reverse = macro(a,  b) { quote(unquote(b) - unquote(a)); };
+			
+			reverse(2 + 2, 10 - 5);
+			`,
+			`(10 - 5) - (2 + 2)`,
+		},
+	}
+
+	for _, tt := range tests {
+		expected := testParseProgram(tt.expected)
+		program := testParseProgram(tt.input)
+
+		env := object.NewEnvironment()
+		DefineMacros(program, env)
+		expanded := ExpandMacros(program, env)
+
+		if expanded.String() != expected.String() {
+			t.Errorf("note equal. wnat=%q, got=%q", expected.String(), expanded.String())
+		}
+	}
+}
