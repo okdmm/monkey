35b6bdde31c3bafc213a9d885b2d761f22f56a77
okada <kirinnremonn1122@gmail.com>
2019-01-31 17:35:14 +0900

    expand repl

--- repl/repl.go
+++ repl/repl.go
@@ -20,6 +20,7 @@ func Start(in io.Reader, out io.Writer) {
 		fmt.Printf(PROMT)
 		scanned := scanner.Scan()
 		env := object.NewEnvironment()
+		macroEnv := object.NewEnvironment()
 		if !scanned {
 			return
 		}
@@ -34,7 +35,10 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		evaluated := evaluator.Eval(program, env)
+		evaluator.DefineMacro(program, macroEnv)
+		expand := evaluator.ExpandMacro(program, macroEnv)
+
+		evaluated := evaluator.Eval(expand, env)
 		if evaluated != nil {
 			io.WriteString(out, evaluated.Inspect())
 			io.WriteString(out, "\n")
