f91e5894330248ed4f5e9986a65cd95fc3378e28
okada <kirinnremonn1122@gmail.com>
2019-03-28 18:32:25 +0900

    emit OpAdd instructions

--- compiler/compiler.go
+++ compiler/compiler.go
@@ -1,6 +1,8 @@
 package compiler
 
 import (
+	"fmt"
+
 	"github.com/okdmm/monkey/ast"
 	"github.com/okdmm/monkey/code"
 	"github.com/okdmm/monkey/object"
@@ -36,6 +38,13 @@ func (c *Compiler) Compile(node ast.Node) error {
 		err = c.Compile(node.Right)
 		if err != nil {
 			return err
+		}
+
+		switch node.Operator {
+		case "+":
+			c.emit(code.OpAdd)
+		default:
+			return fmt.Errorf("unknown operator %s", node.Operator)
 		}
 
 	case *ast.IntegerLiteral:
